90069eeef848c95a18805e6a4774202555d8df29
Álvaro <108766073+agbecker@users.noreply.github.com>
2025-11-27 19:19:59 -0300

    Correct mastermind

--- Examples/mastermind.py
+++ Examples/mastermind.py
@@ -1,3 +1,13 @@
+# Mastermind
+# The player has 6 chances to guess a randomly chosen 4-letter sequence
+# That is any arrangement of the letters ABCDEF
+# Guesses are made by directly inputting the guessed 4 letter sequence, e.g. 'BADF'
+# The guess will appear on the left column group. The right column group shows how good the guess was
+# Each column on the rigth group scores the respective column on the left group
+# - Y means a correct letter in the correct position
+# - E means a correct letter in an incorrect position
+# - N means the letter does not appear in the sequence, or all instances of that letter which do have already been scored
+
 from boardwalk import Game, Board, is_movement, is_placement, get_move_elements
 from random import choice
 import re
@@ -6,37 +16,43 @@ class Mastermind(Game):
     
     def __init__(self, board):
         super().__init__(board)
-        self.secret = ''.join([choice('ABCD') for _ in range(4)])
-        self.max_rounds = board.height
+        self.secret = ''.join([choice('ABCDEF') for _ in range(4)])
+        self.max_turns = board.height
         
     def validate_move(self, move):
-        return bool(re.fullmatch(r'[ABCD]{4}', move))
+        return bool(re.fullmatch(r'[ABCDEF]{4}', move))
     
     def perform_move(self, move):
         for i, c in enumerate(move):
-            self.board.place_piece(f'{c} {self.round}, {i+1}')
+            self.board.place_piece(f'{c} {self.turn-1}, {i}')
             
         # verification
-        counts = {c: self.secret.count(c) for c in 'ABCD'}
+        counts = {c: self.secret.count(c) for c in 'ABCDEF'}
         
+        # Checks correct position
         for i in range(4):
             c = move[i]        
             if c == self.secret[i]:
-                self.board.place_piece(f'Y {self.round}, {i+6}')
+                self.board.place_piece(f'Y {self.turn-1}, {i+5}')
                 counts[c] -= 1
-                
-            elif c in self.secret and counts[c] > 0:
-                self.board.place_piece(f'E {self.round}, {i+6}')
+        # Checks correct letter, wrong position
+        for i in range(4):
+            c = move[i]
+            if c == self.secret[i]:
+                continue
+
+            if c in self.secret and counts[c] > 0:
+                self.board.place_piece(f'E {self.turn-1}, {i+5}')
                 counts[c] -= 1
                 
             else:
-                self.board.place_piece(f'N {self.round}, {i+6}')
+                self.board.place_piece(f'N {self.turn-1}, {i+5}')
             
     def game_finished(self):
-        return self.round >= self.max_rounds or self.get_winner()
+        return self.turn >= self.max_turns or self.get_winner()
         
     def get_winner(self):
-        return 1 if ''.join(self.board.layout[self.round-1][0:4]) == self.secret else None
+        return 1 if ''.join(self.board.layout[self.turn-1][0:4]) == self.secret else None
     
     def next_player(self):
         return self.current_player
